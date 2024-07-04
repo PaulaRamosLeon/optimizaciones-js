4aae40616dbd194d56ae365fcb6409145d542c0d
PaulaRamosLeon <ramosleonpaula@alumnos.cei.es>
2024-07-04 22:07:04 +0200

    pestañas + empezar ightbox

--- JS/pestanas.js
+++ JS/pestanas.js
@@ -1,11 +1,29 @@
 // Paso 1: Declarar variables
 
-const botones = document.querySelectorAll(".opcion");
+const botones = document.querySelectorAll(".titulo");
+const textos = document.querySelectorAll(".texto");
 
 // Paso 2: Funciones
+for (let i = 0; i < botones.length; i++) {
 
-botones.forEach(boton => {
-    boton.addEventListener("click", () => {
-        const
+    botones[i].addEventListener("click", () => {
+        
+        let item = textos[i];
+
+        cerrarTodos();
+
+        item.style = "opacity: 100%";
+        botones[i].classList.add("activo");
     })
-})
+}
+
+
+// Paso 3: Ejecutar código
+function cerrarTodos() {
+    textos.forEach(texto => {
+        texto.style = "opacity: 0%";
+    })
+    botones.forEach(boton => {
+        boton.classList.remove("activo");
+    })
+}
